882f092d6ca509eb03873f7ab36793162816affe
Nina Vo <nbvo@umass.edu>
2025-05-30 20:38:26 -0400

    First commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoKGNVWEXLp1pzJlt2C2vFg3ZLunLlSYE",
-  authDomain: "beginner-stock-journal.firebaseapp.com",
-  projectId: "beginner-stock-journal",
-  storageBucket: "beginner-stock-journal.firebasestorage.app",
-  messagingSenderId: "834782233891",
-  appId: "1:834782233891:web:8ccdcfdb0537d02253b896",
-  measurementId: "G-7KQ7TZVDYW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider, signInWithPopup, signOut };
+export default app;
