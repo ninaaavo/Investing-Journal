a984f4c07f1f2c89561a45bb8832b431ddc5da4e
Nina Vo <nbvo@umass.edu>
2025-08-05 13:13:16 -0400

    remove console.log

--- src/utils/calculateWinRate.js
+++ src/utils/calculateWinRate.js
@@ -9,20 +9,16 @@ export async function calculateWinRate() {
   try {
     const userRef = doc(db, "users", user.uid);
     const userSnap = await getDoc(userRef);
-        console.log("before usersnap")
 
     if (!userSnap.exists()) return "N/A";
-    console.log("after usersnap")
 
     const data = userSnap.data();
     const winCount = data.winCount ?? 0;
     const lossCount = data.lossCount ?? 0;
     const total = winCount + lossCount;
-    console.log("wincount is", winCount, "loss count is", lossCount)
     if (total === 0) return "N/A";
 
     const winRate = (winCount / total) * 100;
-    console.log("you winrate is", winRate)
     return `${winRate.toFixed(1)}%`;
   } catch (error) {
     console.error("Error calculating win rate:", error);
